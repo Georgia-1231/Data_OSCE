f02590210fdde70143676c2e96b40850e1fa7281
Georgia-1231 <georgiachavezus@gmail.com>
2024-12-12 15:55:47 -0500

    Update index.js
    Nueva

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const jsonServer = require("json-server"); 
+const fs = require('fs');
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("v_adjudicaciones_v2.json");
-const middlewares = jsonServer.defaults(); 
-const port = process.env.PORT || 361; 
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 361;
 
-server.use(middlewares); 
-server.use(router);
+server.use(middlewares);
+
+// Manejar datos grandes en streaming
+server.get('/data', (req, res) => {
+    const readStream = fs.createReadStream('v_adjudicaciones_v2.json');
+    readStream.pipe(res);
+});
 
 server.listen(port, () => {
     console.log(`JSON Server is running on port ${port}`);
